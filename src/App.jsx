1f1e2ebc0168c63acf2c61101ea9802caf01ea3d
soala144 <soala144@gmail.com>
2025-05-03 17:28:57 -0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Home from "./Components/Home";
 
 const App = () => {
-  return <div>Cook</div>;
+  return (
+    <div
+      className="min-h-screen flex-col flex items-center justify-center bg-center bg-cover bg-no-repeat"
+      style={{ background: "url('/image.png'), #0F0E0E" }}
+    >
+      <Home />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-white text-2xl">Cook</h1>
+    </div>
+  );
+};
+
+export default Home;
